4e3ad3688ed506cd6532d4ec2ee6c4fc9dab4926
ri88faiMuhamad <86636897+ri88fai@users.noreply.github.com>
2021-08-16 11:37:03 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,28 +1,55 @@
 package com.tutorial;
 
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("======== Nested IF");
-        Scanner inputUser;
-        inputUser = new Scanner(System.in);
+        System.out.println("============ Training Nested if");
+        int a,b,c;
+        a = 1;
+        b = 2;
+        c = 3;
 
-        int a,b;
-        System.out.print("Please enter value a = ");
-        a = inputUser.nextInt();
-        System.out.print("Please enter value b = ");
-        b = inputUser.nextInt();
+        System.out.println("----------- Start Program");
+        if (a == 7){
+            System.out.println("Statement 1");
+        } else {
+            if ( b == 20){
+                System.out.println("Statement 4");
+                } else {
+                if (c == 3){
+                    System.out.println("Statement 3");
+                } else {
+                    System.out.println("Statement 2");
+                }
+            }
+        }
+        System.out.println("----------- Finish Program");
+        System.out.print("\n");
 
-        if (a == 5){
-            if (b == 11){
-                System.out.println("This is statement (a == 5 and b == 11)");
+        System.out.println("----------- Start Program");
+        int value1, value2, value3;
+
+        value1 = 100;
+        value2 = 200;
+        value3 = 300;
+
+        if (value1 == 100){
+            if (value2 == 400){
+                if (value3 == 300){
+                    System.out.println("Statement 2");
+                } else {
+                    if (value3 == value1){
+                        System.out.println("Statement 5");
+                    } else {
+                        System.out.println("Statement 3");
+                    }
+                }
             } else {
-                System.out.println("This is statement (a == 5 and b not 11)");
+                System.out.println("Statement 1");
             }
         } else {
-            System.out.println("This is statement value a = wrong");
+            System.out.println("Statement 4");
         }
+        System.out.println("----------- Finish Program");
 
     }
 }
